d1a14860333f675baad216522c59686d9d7bd3e0
yasuyasu-ot <y.otsubo.dss@gmail.com>
2020-01-22 01:54:38 +0900

    blogコントローラーと画面の雛形作成

--- blog_app/config/routes.rb
+++ blog_app/config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'blogs/index'
+  get 'blogs/show'
+  get 'blogs/new'
+  get 'blogs/create'
+  get 'blogs/edit'
+  get 'blogs/update'
+  get 'blogs/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
