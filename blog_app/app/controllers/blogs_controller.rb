9d8094ff73048f8f3129ea3ec3e654b6ba18ec47
yasuyasu-ot <y.otsubo.dss@gmail.com>
2020-01-26 15:43:34 +0900

    blogオブジェクトをidで取得するメソッドの共通化

--- blog_app/app/controllers/blogs_controller.rb
+++ blog_app/app/controllers/blogs_controller.rb
@@ -4,7 +4,7 @@ class BlogsController < ApplicationController
   end
 
   def show
-    @blog = Blog.find(params[:id])
+    set_blog
   end
 
   def new
@@ -23,11 +23,11 @@ class BlogsController < ApplicationController
   end
 
   def edit
-    @blog = Blog.find(params[:id])
+    set_blog
   end
 
   def update
-    @blog = Blog.find(params[:id])
+    set_blog
     if @blog.update(blog_params)
       redirect_to blogs_path, notice: "ブログを編集しました！"
     else
@@ -42,5 +42,9 @@ class BlogsController < ApplicationController
 
   def blog_params
     params.require(:blog).permit(:title, :content)
+  end
+
+  def set_blog
+    @blog = Blog.find(params[:id])
   end
 end
