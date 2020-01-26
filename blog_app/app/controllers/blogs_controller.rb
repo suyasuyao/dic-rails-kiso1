941b73fb97c979eff1206b7577ebab9378524a78
yasuyasu-ot <y.otsubo.dss@gmail.com>
2020-01-26 22:33:22 +0900

    blogmodelに確認画面を追加

--- blog_app/app/controllers/blogs_controller.rb
+++ blog_app/app/controllers/blogs_controller.rb
@@ -14,12 +14,14 @@ class BlogsController < ApplicationController
 
   def create
     @blog = Blog.new(blog_params)
-    if @blog.save
-      # 一覧画面へ遷移して"ブログを作成しました！"とメッセージを表示します。
-      redirect_to blogs_path, notice: "ブログを作成しました！"
-    else
-      # 入力フォームを再描画します。
+    if params[:back]
       render :new
+    else
+      if @blog.save
+        redirect_to blogs_path, notice: "ブログを作成しました！"
+      else
+        render :new
+      end
     end
   end
 
@@ -37,6 +39,10 @@ class BlogsController < ApplicationController
   def destroy
     @blog.destroy
     redirect_to blogs_path, notice: "ブログを削除しました！"
+  end
+
+  def confirm
+    @blog = Blog.new(blog_params)
   end
 
   private
