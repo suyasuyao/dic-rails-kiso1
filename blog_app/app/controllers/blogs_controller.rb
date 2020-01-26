fcc20b2b34b1a14e246dea1e7a2517e2e3deeb75
yasuyasu-ot <y.otsubo.dss@gmail.com>
2020-01-26 15:39:46 +0900

    validate失敗した際にエラーメッセージを出す

--- blog_app/app/controllers/blogs_controller.rb
+++ blog_app/app/controllers/blogs_controller.rb
@@ -13,8 +13,13 @@ class BlogsController < ApplicationController
 
   def create
     @blog = Blog.new(blog_params)
-    @blog.save
-    redirect_to blogs_path
+    if @blog.save
+      # 一覧画面へ遷移して"ブログを作成しました！"とメッセージを表示します。
+      redirect_to blogs_path, notice: "ブログを作成しました！"
+    else
+      # 入力フォームを再描画します。
+      render :new
+    end
   end
 
   def edit
